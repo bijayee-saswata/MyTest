8a2f0e3711c8f6d614c0c1f714d113e48e237f6b
bijayee-saswata <saswat.sipun@gmail.com>
2018-04-01 00:38:45 +0530

    sign in with email

--- app/src/main/java/com/example/bg/mytest/signUpActivity.java
+++ app/src/main/java/com/example/bg/mytest/signUpActivity.java
@@ -18,25 +18,32 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class signUpActivity extends AppCompatActivity {
 
     private TextView fsignIn;
     private Button signup;
-    private EditText email,password;
+    private EditText name,email,password;
     private ProgressBar progressBar1;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListner;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         fsignIn=(TextView)findViewById(R.id.signInt);
         signup=(Button) findViewById(R.id.signUp);
+        name= (EditText) findViewById(R.id.name);
         email =(EditText)findViewById(R.id.semail);
         password =(EditText)findViewById(R.id.spassword);
         progressBar1 = (ProgressBar) findViewById(R.id.SprogressBar1);
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         fsignIn.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -79,23 +86,29 @@ public class signUpActivity extends AppCompatActivity {
 
     private void signUp()
     {
-        String userId = email.getText().toString();
-        String pass = password.getText().toString();
+        final String userName = name.getText().toString().trim();
+        final String Email = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
 
-        if(TextUtils.isEmpty(userId) || TextUtils.isEmpty(pass))
+        if(TextUtils.isEmpty(Email) || TextUtils.isEmpty(pass))
         {
             Toast.makeText(getApplicationContext(), "Filled are Empty.",
                     Toast.LENGTH_SHORT).show();
         }
         else
         {
-            mAuth.createUserWithEmailAndPassword(userId, pass)
+            mAuth.createUserWithEmailAndPassword(Email, pass)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
-                                 FirebaseUser user = mAuth.getCurrentUser();
+
+                                HashMap<String,String> dataMap = new HashMap<String,String>();
+                                dataMap.put("Name" , userName);
+                                dataMap.put("Email" ,Email);
+                                mDatabase.push().setValue(dataMap);
+                                        FirebaseUser user = mAuth.getCurrentUser();
                                 progressBar1.setVisibility(View.INVISIBLE);
                                 Toast.makeText(getApplicationContext(), "Success",
                                         Toast.LENGTH_SHORT).show();
